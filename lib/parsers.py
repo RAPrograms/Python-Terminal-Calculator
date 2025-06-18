99bd155393b310363295c89ae4af0cbd76b49e2f
Dog2657 <RAPrograms@outlook.com>
2025-06-18 21:03:07 +0100

    Added main parser

--- lib/parsers.py
+++ lib/parsers.py
@@ -28,7 +28,7 @@ def number(equation: str, start: int = 0) -> tuple[float, int] | None:
     return (numb, length)
 
 
-def parse_operator(equation: str, start: int = 0) -> tuple[list[Any], int]:
+def operator(equation: str, start: int = 0) -> tuple[list[Any], int]:
     match(equation[start]):
         case "^":
             return (Operators.power, 1)
@@ -49,3 +49,52 @@ def parse_operator(equation: str, start: int = 0) -> tuple[list[Any], int]:
        
         case _:
             return None
+        
+
+
+def parse(equation: str, start: int = 0) -> tuple[list[Any], int, dict[str, list[int]]]:
+    assert start < len(equation), "Invalid Starting Index"
+    output = []
+    map = {
+        Operators.addition: [],
+        Operators.subtraction: [],
+        Operators.devision: [],
+        Operators.multiplication: [],
+        Operators.power: [],
+        "()": []
+    }
+
+    i = start
+    while i < len(equation):
+        char = equation[i]
+        if(char == ")"):
+            return (output, i - start, map)
+
+        if(char == "("):
+            data, length, sub_map = parse(equation, i+1)
+            i+= length + 1
+            output.append((data, sub_map))
+            map["()"].append(len(output) - 1)
+            continue
+
+
+        numb_results = number(equation, i)
+        if(numb_results):
+            i += numb_results[1]
+            output.append(numb_results[0])
+            if(numb_results[0] < 0):
+                map[Operators.subtraction].append(len(output))
+            continue
+
+
+        operator_results = operator(equation, i)
+        if(operator_results):
+            i += operator_results[1]
+            map[operator_results[0]].append(len(output))
+            output.append(operator_results[0])
+            continue
+
+        i+= 1
+
+    return (output, i - start, map)
+

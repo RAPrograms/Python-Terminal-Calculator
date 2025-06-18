2aab70ba869597927eb27681debb0239d7f1f33f
Dog2657 <RAPrograms@outlook.com>
2025-06-18 20:54:40 +0100

    Accept user input

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import sys
+
+def get_inputted_equation():
+    result = "".join(sys.argv[1:])
+    if(result == ""):
+        result = input("What's the problem?\n")
+    
+    return result
+
+
+if __name__ == "__main__":
+    equation = get_inputted_equation()
+    print(equation)

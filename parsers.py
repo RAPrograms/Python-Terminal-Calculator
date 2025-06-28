24dcf3e96ef1822d7fbf00932fda9ed1d4a5742f
Jameslol09 <86912398+Jameslol09@users.noreply.github.com>
2025-06-28 22:33:20 +0100

    Add files via upload

---
+++ parsers.py
@@ -0,0 +1,112 @@
+from .enums import Operators
+from math import pi as PI
+from typing import Any
+
+def number(equation: str, start: int = 0) -> tuple[float, int] | None:
+    buffer = ""
+    percentage = False
+
+    for i in range(start, len(equation)):
+        char = equation[i]
+        if(char == "%"):
+            percentage = True
+            break
+
+        if(char not in "0123456789."):
+            break
+
+        buffer += char
+
+    if(buffer == ""):
+        return None
+
+    length = len(buffer)
+    numb = float(buffer)
+    if(percentage):
+        numb /= 100
+        length += 1
+
+    return (numb, length)
+
+
+def operator(equation: str, start: int = 0) -> tuple[Operators, int]:
+    match(equation[start]):
+        case "^":
+            return (Operators.power, 1)
+
+        case "+":
+            return (Operators.addition, 1)
+
+        case "-":
+            return (Operators.subtraction, 1)
+
+        case "/":
+            return (Operators.devision, 1)
+
+        case "*":
+            if(equation[start:start+2] == "**"):
+                return (Operators.power, 2)
+            return (Operators.multiplication, 1)
+       
+        case _:
+            return None
+        
+def constant(equation: str, start: int = 0) -> tuple[Any, int]:
+    if(equation[start:start+2].lower() == "pi"):
+        return (PI, 2)
+
+
+def parse(equation: str, start: int = 0) -> tuple[list[Any], int, dict[str, list[int]]]:
+    assert start < len(equation), "Invalid Starting Index"
+    output = []
+    map = {
+        Operators.addition: [],
+        Operators.subtraction: [],
+        Operators.devision: [],
+        Operators.multiplication: [],
+        Operators.power: [],
+        "()": []
+    }
+
+    i = start
+    while i < len(equation):
+        char = equation[i]
+        if(char == ")"):
+            return (output, i - start, map)
+
+        if(char == "("):
+            data, length, sub_map = parse(equation, i+1)
+            i+= length + 2
+            output.append((data, sub_map))
+            map["()"].append(len(output) - 1)
+            continue
+
+
+        numb_results = number(equation, i)
+        if(numb_results):
+            i += numb_results[1]
+            output.append(numb_results[0])
+            if(numb_results[0] < 0):
+                map[Operators.subtraction].append(len(output))
+            continue
+
+
+        operator_results = operator(equation, i)
+        if(operator_results):
+            i += operator_results[1]
+            map[operator_results[0]].append(len(output))
+            output.append(operator_results[0])
+            continue
+
+
+        constant_results = constant(equation, i)
+        if(constant_results):
+            i += constant_results[1]
+            output.append(constant_results[0])
+            continue
+
+        i+= 1
+
+    print(output)
+    return (output, i - start, map)
+
